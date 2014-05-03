c6ff98dface2be746a69b5d678905361bbd99d1f
Jon Abrams <jon@jonabrams.com>
2014-05-02 22:03:27 -0700

    Some cleanup

--- front/js/controllers/tweetsCtrl.js
+++ front/js/controllers/tweetsCtrl.js
@@ -1,13 +1,4 @@
 angular.module('tweetDemo')
-.controller('tweetsController', function ($scope, $http) {
-
-  if (preloadedData) {
-    $scope.tweets = preloadedData.tweets;
-    preloadedData = null;
-  } else {
-    $http.get('/api/tweets').then(function (res) {
-      $scope.tweets = res.data.tweets;
-    });
-  }
-
+.controller('tweetsController', function ($scope, $http, data) {
+  $scope.tweets = data.tweets;
 });
--- front/js/front-app.js
+++ front/js/front-app.js
@@ -1,13 +1,36 @@
+var dataLoaderRunner = [
+  'dataLoader',
+  function (dataLoader) {
+    return dataLoader();
+  }
+];
+
 angular.module('tweetDemo', ['ngRoute'])
 .config(function ($routeProvider, $locationProvider) {
   $routeProvider
   .when('/tweets', {
     templateUrl: '/html/tweets/getIndex.html',
-    controller: 'tweetsController'
+    controller: 'tweetsController',
+    resolve: {
+      data: dataLoaderRunner
+    }
   })
   .otherwise({
     redirectTo: '/tweets'
   });
 
   $locationProvider.html5Mode(true);
+})
+.service('dataLoader', function ($location, $http) {
+  return function () {
+    if (preloadedData) {
+      var data = preloadedData;
+      preloadedData = null;
+      return data;
+    } else {
+      return $http.get( '/api' + $location.path() ).success(function (data) {
+        return data;
+      });
+    }
+  };
 });
